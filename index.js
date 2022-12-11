0c15cd893aa7e16b68d467e7b3d576b17294a397
Vito <vitocarbone94@gmail.com>
2022-12-11 17:41:38 +0100

    A

---
+++ index.js
@@ -0,0 +1,68 @@
+const charactersArray = ["g", "n", "u", "z", "d"]
+switch (charactersArray){
+    case "a":
+        console.log("1");
+    break;
+    case "b":
+        console.log("12");
+    break;
+    case "c":
+        console.log("3");
+    break;
+    case "d":
+        console.log("4");
+    break;
+    case "e":
+        console.log("5");
+    break;
+    case "f":
+        console.log("6");
+    break;
+    case "g":
+        console.log("7");
+    break;
+    case "h":
+        console.log("8");
+    break;
+    case "i":
+        console.log("9");
+    break;
+    case "l":
+        console.log("10");
+    break;
+    case "m":
+        console.log("11");
+    break;
+    case "n":
+        console.log("12");
+    break;
+    case "o":
+        console.log("13");
+    break;
+    case "p":
+        console.log("14");
+    break;
+    case "q":
+        console.log("15");
+    break;
+    case "r":
+        console.log("16");
+    break;
+    case "s":
+        console.log("17");
+    break;
+    case "t":
+        console.log("18");
+    break;
+    case "u":
+        console.log("19");
+    break;
+    case "v":
+        console.log("20");
+    break;
+    case "z":
+        console.log("21");
+    break;
+    
+    
+}
